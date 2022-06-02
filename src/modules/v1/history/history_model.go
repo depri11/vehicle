ef1b5b3a9a2cf1b106e82371d354a836fe6859cf
devriwirlandika <80185253+depri11@users.noreply.github.com>
2022-06-02 19:09:42 +0700

    delete unused model struct

--- src/modules/v1/history/history_model.go
+++ src/modules/v1/history/history_model.go
@@ -18,16 +18,4 @@ type Historys struct {
 	UpdatedAt  time.Time
 }
 
-type CreateHistorys struct {
-	ID         int        `json:"id" gorm:"primaryKey"`
-	UserID     int        `json:"user_id"`
-	Name       string     `json:"name"`
-	Duration   string     `json:"duration"`
-	Prepayment string     `json:"prepayment"`
-	Returned   bool       `json:"returned"`
-	User       users.User `json:"user"`
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-}
-
 type Historyss []Historys
