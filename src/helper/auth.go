6930d2dc23a999620274598b034f956dd7265e8d
devriwirlandika <80185253+depri11@users.noreply.github.com>
2022-06-11 01:37:35 +0700

    add id in claims jwt

--- src/helper/auth.go
+++ src/helper/auth.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"errors"
 	"os"
 	"time"
 
@@ -8,15 +9,17 @@ import (
 )
 
 type claims struct {
+	Id    int    `json:"id"`
 	Email string `json:"email"`
 	jwt.StandardClaims
 }
 
-func NewToken(email string) *claims {
+func NewToken(id int, email string) *claims {
 	return &claims{
+		Id:    id,
 		Email: email,
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
+			ExpiresAt: time.Now().Add(time.Hour * 5).Unix(),
 		},
 	}
 }
@@ -26,14 +29,39 @@ func (c *claims) Create() (string, error) {
 	return token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
 }
 
-func CheckToken(token string) (bool, error) {
+func CheckToken(token string) (*claims, error) {
 	tokens, err := jwt.ParseWithClaims(token, &claims{}, func(t *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("JWT_SECRET_KEY")), nil
 	})
 
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 
-	return tokens.Valid, nil
+	// claims, ok := tokens.Claims.(*claims)
+	claims, ok := tokens.Claims.(*claims)
+	if !ok {
+		err = errors.New("couldn't parse claims")
+		return nil, err
+	}
+
+	return claims, nil
 }
+
+// func CheckToken(token string) (bool, error) {
+// 	tokens, err := jwt.ParseWithClaims(token, &claims{}, func(t *jwt.Token) (interface{}, error) {
+// 		return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+// 	})
+
+// 	if err != nil {
+// 		return false, err
+// 	}
+
+// 	// claims, ok := tokens.Claims.(*claims)
+
+// 	// if ok && tokens.Valid {
+// 	// 	return uint(claims.ID), nil
+// 	// }
+
+// 	return tokens.Valid, nil
+// }
