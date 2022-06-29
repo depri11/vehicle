48f9dc3cff52f75b2505fe8926cb30e6a0916f6a
devriwirlandika <80185253+depri11@users.noreply.github.com>
2022-06-29 22:13:10 +0700

    deploy rev

--- src/database/db.go
+++ src/database/db.go
@@ -7,16 +7,15 @@ import (
 	"time"
 
 	"github.com/depri11/vehicle/src/database/models"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func SetupDB() (*gorm.DB, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return nil, err
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	return nil, err
+	// }
 
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
